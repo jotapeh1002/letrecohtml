78342e698b08494b03e8b4889ed43c09904be955
jotapeh <alisssongomes22@gmail.com>
2024-10-07 17:52:24 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // ajuste conforme a estrutura do seu projeto
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
